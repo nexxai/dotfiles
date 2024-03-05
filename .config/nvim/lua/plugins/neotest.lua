edc569d1d75100c5070cccc1d2293e099b58bec5
nexxai <nexxai@gmail.com>
2024-03-04 23:55:27 -0700

    Using replacement version of neopest

---
+++ .config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,33 @@
+vim.keymap.set("n", "<leader>t", "function() require('neotest').run.run() end")
+vim.keymap.set("n", "<leader>tt", "function() require('neotest').run.run(vim.fn.expand('%')) end")
+vim.keymap.set("n", "<leader>tf", function()
+  require("neotest").run.run({ suite = true })
+end)
+vim.keymap.set("n", "<leader>ts", "function() require('neotest').summary.open() end")
+
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      -- "theutz/neotest-pest",
+      "V13Axel/neotest-pest",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-pest")({
+            pest_cmd = function()
+              return "vendor/bin/pest"
+            end,
+            sail_enabled = function()
+              return false
+            end,
+          }),
+        },
+      })
+    end,
+  },
+}
