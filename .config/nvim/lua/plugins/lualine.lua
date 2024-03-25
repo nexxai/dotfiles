30c973e6168478d955426387cf59400cb89db764
nexxai <nexxai@gmail.com>
2024-03-24 22:15:32 -0600

    Show a better mode when in visual-multi as an indicator that we're still in it

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,11 +4,29 @@ return {
   event = "VeryLazy",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = function(_, opts)
+    vim.g.VM_set_statusline = 0
+    vim.g.VM_silent_exit = 1
     table.insert(opts.sections.lualine_x, "😄")
   end,
   config = function()
     require("lualine").setup({
       sections = {
+        lualine_a = {
+          {
+            "mode",
+            -- If visual multi is active, show the mode in white and red
+            -- and show it as the active mode
+            color = function()
+              return {
+                fg = vim.b["visual_multi"] and "white" or nil,
+                bg = vim.b["visual_multi"] and "red" or nil,
+              }
+            end,
+            fmt = function(mode)
+              return vim.b["visual_multi"] and "VM - MULTI" or mode
+            end,
+          },
+        },
         lualine_x = { "require'wttr'.text" },
       },
     })
